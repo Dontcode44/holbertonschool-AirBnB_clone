69088b48c79cf07c30d6d0dbe60484d67eaa8e36
DanielaIH <4427@holbertonschool.com>
2022-07-01 13:37:45 -0500

    First version base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+import uuid
+from datetime import datetime
+"""holbertonschool-AirBnB_clone"""
+
+
+class BaseModel():
+    """BaseModel - class for all common attributes/methods"""
+    def __init__(self, id=None):
+        """__init__ - inicialize the attributes"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now().isoformat("T", "microseconds")
+        self.updated_at = datetime.now().isoformat("T", "microseconds")
+    
+    def __str__(self):
+        """__str__ - print class name, id and dict"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """save - updates the public instance attribute updated_at"""
+        self.updated_at = datetime.now().isoformat("T", "microseconds")
+        
+    def to_dict(self):
+        """to_dict - returns a dictionary containing all keys/values
+        of __dict__ of the instance"""
+        
+#2022-07-01T18:15:47.144062
