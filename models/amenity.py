cf8109e11b1e77feeb49acff36c033ed70a3eae0
Dontcode44 <4659@holbertonschool.com>
2022-07-06 16:03:55 -0500

    Classes

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Amenity class from BaseModel"""
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """Atributes of the class"""
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""City class from BaseModel"""
+
+from models.base_model import BaseModel
+
+class City(BaseModel):
+    """Atributes of the class"""
+    state_id = ""
+    name = ""
