15dba3b4b26faa15a92ebb4333bb12fdca553311
DanielaIH <4427@holbertonschool.com>
2022-07-05 20:59:07 -0700

    without test

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,4 +1,1 @@
-#!/usr/bin/python3
-import unittest
-from console import HBNBCommand
-"""unitest for console.py class HBNBCommand"""
+anything
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,6 +1,1 @@
-#!/usr/bin/python3
-import unittest
-import models
-from models import storage
-from models.base_model import BaseModel
-"""unitest for file_storage.py class file storage"""
+anything
