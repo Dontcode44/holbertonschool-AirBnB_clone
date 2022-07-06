3d1bc2ed7ef931f40063d44b058d7a86c5a986c1
DanielaIH <4427@holbertonschool.com>
2022-07-05 20:45:51 -0700

    test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -10,16 +10,11 @@ from models.base_model import BaseModel
 
 class Tests_BaseModel(unittest.TestCase):
     """Tests for FileStorage class"""
-    my_model = BaseModel()
 
     def testing(self):
         """testing - Test dict"""
-        self.assertIsNotNone(self.my_model)
-        self.assertIsInstance(storage.all(), dict)
-        self.assertIsInstance(self.my_model, BaseModel)
-        self.assertIsInstance(self.my_model.updated_at, datetime)
-        self.assertIsInstance(self.my_model.created_at, datetime)
-        self.assertIsInstance(self.my_model.id, str)
+        my_model = BaseModel()
+        self.assertIsInstance(my_model, BaseModel)
 
 
 if __name__ == '__main__':
