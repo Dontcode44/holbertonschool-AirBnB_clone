88f26d365a01ea611f1494b4934bceac94a78f83
Dontcode44 <4659@holbertonschool.com>
2022-07-05 21:18:00 -0500

    Test

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""unitest for basemodel.py class BaseModel"""
+
+
+from datetime import datetime
+import unittest
+from models import storage
+from models.base_model import BaseModel
+
+
+class Tests_BaseModel(unittest.TestCase):
+    """Tests for FileStorage class"""
+    my_model = BaseModel()
+
+    def testing(self):
+        """testing - Test dict"""
+        self.assertIsNotNone(self.my_model)
+        self.assertIsInstance(storage.all(), dict)
+        self.assertIsInstance(self.my_model, BaseModel)
+        self.assertIsInstance(self.my_model.updated_at, datetime)
+        self.assertIsInstance(self.my_model.created_at, datetime)
+        self.assertIsInstance(self.my_model.id, str)
+
+    def test_exist_class(self):
+        """Test if the class exist"""
+        self.assertEqual(str(type(self.my_model)),
+        "<class 'models.base_model.BaseModel'>")
+
+if __name__ == '__main__':
+    unittest.main()
