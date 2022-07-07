e8ed19fc1b06c9290ca67cf01db3cf6dabc0e39d
Dontcode44 <4659@holbertonschool.com>
2022-07-07 14:11:02 -0500

    Test User

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,45 +1,57 @@
 #!/usr/bin/python3
-""" Unit tests for Amenity class """
-
+""" Test file for User class """
 import unittest
+from models.base_model import BaseModel
 from models.user import User
-import pep8
+from models import storage
+from models.engine.file_storage import FileStorage
+import datetime
 
 
-class Test_User(unittest.TestCase):
-    """Base class tests"""
+class TestUser(unittest.TestCase):
+    def setUp(self):
+        """
+        the setUp() method raises an exception
+        while the test is running
+        """
+        pass
 
-    def test_pep8_conformance_user(self):
-        """Test that we conform to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/user.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+    def tearDown(self):
+        """
+        a tearDown() method that tidies up after
+        the test method has been run
+        """
+        pass
 
-    def test_hasattr(self):
-        """ Test if an object has an attribute """
+    def test_user_instantiation(self):
+        """User instance attributes"""
         user = User()
-        self.assertTrue(hasattr(user, "id"))
-        self.assertTrue(hasattr(user, "created_at"))
-        self.assertTrue(hasattr(user, "updated_at"))
-        self.assertTrue(hasattr(user, "__init__"))
-
-    def test_type_user(self):
-        """ Test the type of the attribute """
-        user = User()
-        self.assertEqual(type(user.email), str)
-        self.assertEqual(type(user.password), str)
-        self.assertEqual(type(user.first_name), str)
-        self.assertEqual(type(user.last_name), str)
-
-    def test_instance(self):
-        """ Test if is instance """
-        user = User()
+        self.assertEqual(str(type(user)), "<class 'models.user.User'>")
         self.assertIsInstance(user, User)
+        self.assertTrue(issubclass(type(user), BaseModel))
 
-    def test_docstring(self):
-        """ Test documentation """
-        self.assertIsNotNone(User.__doc__)
+    def test_user_Attributes(self):
+        """User Attributes exist"""
+        user = User()
+        self.assertTrue(hasattr(user, 'email'))
+        self.assertTrue(hasattr(user, 'password'))
+        self.assertTrue(hasattr(user, 'first_name'))
+        self.assertTrue(hasattr(user, 'last_name'))
+        self.assertTrue(hasattr(user, 'id'))
+        self.assertTrue(hasattr(user, 'created_at'))
+        self.assertTrue(hasattr(user, 'updated_at'))
+
+    def test_user_types(self):
+        """User correct attributes' types"""
+        user = User()
+        self.assertIsInstance(user.first_name, str)
+        self.assertIsInstance(user.last_name, str)
+        self.assertIsInstance(user.email, str)
+        self.assertIsInstance(user.password, str)
+        self.assertIsInstance(user.id, str)
+        self.assertIsInstance(user.created_at, datetime.datetime)
+        self.assertIsInstance(user.updated_at, datetime.datetime)
+
 
 if __name__ == '__main__':
     unittest.main()
