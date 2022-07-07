e647ad2507ad83e5203f5376221a3cc7b8ad6cfa
DanielaIH <4427@holbertonschool.com>
2022-07-06 23:52:01 -0500

    UPDATE FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -41,7 +41,7 @@ class FileStorage():
         if path.exists(self.__file_path):
             all_obj = self.read_json()
         for key, value in all_obj.items():
-            self.__objects[key] = BaseModel(**value)
+            self.__objects[key] = FileStorage.classes[value["__class__"]](**value)
         pass
 
     def read_json(self):
